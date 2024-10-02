f28e143c4d486477e85a04446f026a1e148c94db
Zerumi <afanasyev09032005@gmail.com>
2024-10-02 19:27:59 +0300

    fix: add missing models

--- frontend/src/app/model.ts
+++ frontend/src/app/model.ts
@@ -1,8 +1,31 @@
 export class LoginData {
-    username!: string;
-    password!: string;
+  username!: string;
+  password!: string;
 }
 
 export class AuthResponse {
-    token!: string;
+  token!: string;
+}
+
+export class HumanBeing {
+  id!: number;
+  name!: String;
+  coordinates!: Coordinates;
+  realHero!: Boolean;
+  hasToothpick!: Boolean;
+  car!: Car;
+  mood!: string;
+  impactSpeed!: number;
+  minutesOfWaiting!: number;
+  weaponType!: string;
+  creationDate!: string;
+}
+
+export class Coordinates {
+  x!: number;
+  y!: number;
+}
+
+export class Car {
+  name!: String;
 }
